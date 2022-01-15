4ee18218ad4453dd2e8363abf5fafa4d9d600a83
Saurabh Yadav <saurabh.yadav@skuad.io>
2022-01-15 18:25:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidbroadcastreceiver/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.androidbroadcastreceiver
+
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver: AirplaneModeChangedReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        receiver = AirplaneModeChangedReceiver()
+
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            // Make sure to unregister the broadcast receiver to prevent memory leak.
+            registerReceiver(receiver, it)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
+}
